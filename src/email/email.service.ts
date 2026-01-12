b5cfd8b1478129f656308adaa07a537cbdac4b52
KOMPUTER\semyon <mamoshin_sem@mail.ru>
2026-01-12 20:25:28 +0300

    Bugfix. Correct 'from'

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -13,18 +13,22 @@ export class EmailService {
       throw new InternalServerErrorException('Target Email not found')
     }
 
+    const _secure = this.configService.get<string>('SMTP_SECURE')
+    const user = this.configService.get<string>('SMTP_USER', 'your_gmail@gmail.com')
+
     const transporter = nodemailer.createTransport({
-      host: this.configService.get<string>('SMTP_HOST') || 'smtp.gmail.com',
-      port: this.configService.get<number>('SMTP_PORT') || 587,
-      secure: false, // true for port 465
+      host: this.configService.get<string>('SMTP_HOST', 'smtp.gmail.com'),
+      port: Number(this.configService.get<number>('SMTP_PORT', 587)),
+      secure: _secure === 'true', // true for port 465
       auth: {
-        user: this.configService.get<string>('SMTP_USER') || 'your_gmail@gmail.com',
-        pass: this.configService.get<string>('SMTP_PASS') || 'your_app_password'
+        user,
+        pass: this.configService.get<string>('SMTP_PASS', 'your_app_password')
       }
     })
 
     try {
       await transporter.sendMail({
+        from: user,
         to: targetEmail,
         subject: dto.theme,
         text: `От: ${dto.email}\n\nТема: ${dto.theme}\n\n${dto.description}`
